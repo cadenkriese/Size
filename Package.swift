dff9904165d669d2c26141788aabe725a17f9a32
Caden Kriese <13630061+cadenkriese@users.noreply.github.com>
2026-07-27 06:41:00 -0600

    Specify lowercase name executable

--- Package.swift
+++ Package.swift
@@ -8,6 +8,9 @@ let package = Package(
     platforms: [
         .macOS(.v26)
     ],
+    products: [
+        .executable(name: "sizes", targets: ["Sizes"])
+    ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.8.2"),
     ],
